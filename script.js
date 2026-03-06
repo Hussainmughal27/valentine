efe4855a26bee8f618b9e3cd3a5633bd8ac4f650
Hussainmughal27 <hussainking2712@gmail.com>
2026-03-06 08:03:15 +0500

    dawoodi

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ moveRandom.addEventListener("mouseenter", function(e){
     moveRandomEl(e.target);
 });
 }
+function openLetter(){
+    const box = document.getElementById("letterBox");
+    const msg = "Haha, silly… did you really think I’d help you?😜 “There’s no other option for you except yes.” 😄";
+
+    const messageElement = document.getElementById("message");
+
+    box.style.display = "block";
+    messageElement.innerHTML = "";
+
+    let i = 0;
+
+    function typeWriter(){
+        if(i < msg.length){
+            messageElement.innerHTML += msg.charAt(i);
+            i++;
+            setTimeout(typeWriter,40);
+        }
+    }
+
+    typeWriter();
+}
+
+document.getElementById("closeLetter").onclick = function(){
+    document.getElementById("letterBox").style.display = "none";
+}
